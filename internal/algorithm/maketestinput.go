299db9d48a238e3c6585e5a067ef55ba288a229c
koushilab2 <temp1.koushi@gmail.com>
2021-05-30 20:39:46 +0530

    Lets Test

--- internal/algorithm/maketestinput.go
+++ internal/algorithm/maketestinput.go
@@ -2,15 +2,22 @@ package MRUApp
 
 func MakeTestInput() *MRU {
 	data := MRU{}
-	addMRUData()
+	addMRUData(&data)
 	return &data
 }
 
-func addMRUData(data MRU) {
+func addMRUData(data *MRU) {
 	content := MRU{
 		State: State{
 			Model: "NewTextFile",
-			Item:  []Item{},
+			Item: []Item{{
+				Label: "file1",
+				Payload: Payload{
+					Content: "Data of the file goes here",
+				},
+			},
+			},
 		},
 	}
+	data.State = content.State
 }
